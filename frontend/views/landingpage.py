2ad66a3540148938f603e3a69e143314682653ba
Initech <inimfonebong001@gmail.com>
2023-11-29 12:08:03 +0000

    cleared all files

--- frontend/views/landingpage.py
+++
@@ -1,6 +0,0 @@
-from views import app_views
-from flask import render_template
-
-@app_views.route('/', methods=['GET'], strict_slashes=True)
-def landingpage():
-    return render_template('landingpage.html')
--- sys_deveops/setup_mysql_dev.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS career_gate_dev_db;
-CREATE USER IF NOT EXISTS 'career_gate_dev'@'localhost' IDENTIFIED BY 'career_gate_dev_pwd';
-GRANT ALL PRIVILEGES ON `career_gate_dev_db`.* TO 'career_gate_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'career_gate_dev'@'localhost';
-FLUSH PRIVILEGES;
