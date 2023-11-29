1569b03f2360d2d7209206af69719b50f630db72
Initech <inimfonebong001@gmail.com>
2023-11-29 15:42:31 +0000

    Removed all files

--- frontend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from views import app_views
-
-app = Flask(__name__)
-app.secret_key = "0cbc6611f5540bd0809a388dc95a615b"
-app.register_blueprint(app_views)
-if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0")
-
--- frontend/views/login.py
+++
@@ -1,8 +0,0 @@
-from views import app_views
-from flask import render_template, request, abort
-import requests
-
-@app_views.route('/login', methods=['GET', "POST"], strict_slashes=True)
-def login():
-
-    return render_template('login.html')
