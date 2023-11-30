b01ed9412f90bca8a7a77b452ba35dd02623e7ad
Initech <inimfonebong001@gmail.com>
2023-11-30 14:25:20 +0000

    remove SQLAlchemy

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@
 initialize the models package
 """
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from models.views import app_views
