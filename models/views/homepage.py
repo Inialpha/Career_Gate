57cea9461c7523ec7946d04a55b5d397524c4a98
initech <inimfonebong001@gmail.com>
2023-12-12 17:27:40 +0000

    linked the admin dashboard with the backend

--- models/views/homepage.py
+++ models/views/homepage.py
@@ -2,11 +2,18 @@ from models.views import app_views
 from flask import render_template, redirect, flash, url_for
 import requests
 from flask_login import current_user, login_required
-
+import models
 
 @app_views.route('/homepage', methods=['GET', 'POST'], strict_slashes=True)
 @login_required
 def homepage():
     """Every users homepage"""
     user = current_user
+    if user.user_type == "admin":
+        from models.interview import Interview
+        from models.resume import Resume
+
+        resumes = models.storage.all(Resume).values()
+        interviews = models.storage.all(Interview).values()
+        return render_template('admindashboard.html', user=user, resumes=resumes, interviews=interviews)
     return render_template('userdashboard.html', user=user);
