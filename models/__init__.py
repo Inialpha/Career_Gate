18c3615eb1382db2ebf2de11abd600bc98206af5
Initech <inimfonebong001@gmail.com>
2024-07-16 12:09:28 +0000

    Implement database migration

--- models/__init__.py
+++ models/__init__.py
@@ -8,8 +8,26 @@ from flask_login import LoginManager
 from models.views import app_views
 import models.views
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from os import getenv
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://{}:{}@{}/{}'.format(
+    getenv('MYSQL_USER'),
+    getenv('MYSQL_PWD'),
+    getenv('MYSQL_HOST'),
+    getenv('MYSQL_DB')
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "app_views.login"
